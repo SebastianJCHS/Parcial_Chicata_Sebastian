d0cff111e5d5cfaedb536ba855724b625bf4ad0d
Sebastian Chicata <sebastian.chicata@gmail.com>
2024-10-07 10:59:47 -0500

    Primer avance

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,45 @@
+public class Cliente {
+    private String RUC;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String clave;
+    
+    public Cliente(){}
+
+    public String getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(String RUC) {
+        this.RUC = RUC;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    public boolean agregarOferta(){
+        return true;
+    }
+}
